244323460e95154279ee60fe544c27f7fc8d4311
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-09-23 21:04:15 -0500

    hover
    100%;

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 function translate_back(info){
 
     let description =info.children.item(0);
+    info.classList.toggle("poster_zoom")
     description.classList.toggle("translate")
 }
 function translate_down(info){
