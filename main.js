8c929adb7f75dc9b065e3138397c776f074cf99b
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-09-24 12:49:44 -0500

    slider

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 
 
 
+  let dots =document.querySelectorAll(".dot");
+   dots.forEach(dot=> dot.addEventListener("click",changeImage));
+
+
+  const mp40Images=['src/imgs/mp40Images/mp40_1.jpg','src/imgs/mp40Images/mp40_2.jpg','src/imgs/mp40Images/mp40_3.jpg','src/imgs/mp40Images/mp40_4.jpg']
+
+    function changeImage()
+    {
+        const innerDots =this.parentNode.querySelectorAll(".dot");
+        innerDots.forEach(dot=> {
+
+            if (dot!=this){
+                dot.style.backgroundColor= "#f5ead3";
+            }
+        })
+        this.style.backgroundColor="#e09a2f";
+         const dotsArray=[...innerDots];
+        const key =this.parentNode.dataset.key
+        if (key == "mp40"){
+
+
+            this.parentNode.style.backgroundImage=`url(${mp40Images[dotsArray.indexOf(this)]})`
+        }
+
+    }
     function show_description(info){
 
         const description =info.children.item(0);
