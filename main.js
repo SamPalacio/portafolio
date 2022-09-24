50937c439e1b069a1b58a2ff32ae78f74b5dd566
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-09-23 22:21:53 -0500

    gameplay

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 
 
 
-function translate_back(info){
+    function show_description(info){
 
-    let description =info.children.item(0);
-    info.classList.toggle("poster_zoom")
-    description.classList.toggle("translate")
-}
-function translate_down(info){
+        const description =info.children.item(0);
+        info.classList.toggle("poster_zoom")
+        description.classList.toggle("translate")
+    }
 
+    function show_video(id_video){
+        const video_screen =document.getElementById(id_video);
+        video_screen.classList.toggle("show")
+        let iframe_tag = video_screen.querySelector( 'iframe');
+        let video_tag = video_screen.querySelector( 'video' );
+        if ( iframe_tag) {
+            let iframeSrc = iframe_tag.src;
+            iframe_tag.src = iframeSrc;
+        }
+        if ( video_tag) {
+            video_tag.pause();
+        }
+    }
 
-    let description =info.children.item(0);
-    description.classList.toggle("translate")
-}
 
