41f7c08680f5823adcfe394217988c84a1cff8b8
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-09-23 20:24:48 -0500

    hover effect

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+function translate_back(info){
+
+    let description =info.children.item(0);
+    description.classList.toggle("translate")
+}
+function translate_down(info){
+
+
+    let description =info.children.item(0);
+    description.classList.toggle("translate")
+}
+
