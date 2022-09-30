19d608c797770b013dd1a15bb59d6ae3536b6e6b
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-09-30 00:08:02 -0500

    hamburguer_menu

--- main.js
+++ main.js
@@ -4,8 +4,16 @@
     let dots =document.querySelectorAll(".dot");
     let firstDots = document.querySelectorAll(".dot.first");
     let sectionBtns =document.querySelectorAll(".options_bar button");
-    let cvBtn =document.querySelector(".cv")
+    let hamburgerBtns =document.querySelectorAll(".options button");
+    let cvBtn =document.querySelector(".cv");
+    let menu=document.querySelector(".menu");
+    let close=document.querySelector(".close");
+    const menu_bar = document.querySelector(".menu_side");
 
+    const mp40Images=['src/imgs/mp40Images/mp40_1.jpg','src/imgs/mp40Images/mp40_2.jpg','src/imgs/mp40Images/mp40_3.jpg','src/imgs/mp40Images/mp40_4.jpg'];
+    const gameboyImages=['src/imgs/gameboyImages/gameboy_1.jpg','src/imgs/gameboyImages/gameboy_2.jpg','src/imgs/gameboyImages/gameboy_3.jpg'];
+    const saloonImages=['src/imgs/saloonImages/saloon_1.jpg','src/imgs/saloonImages/saloon_2.jpg','src/imgs/saloonImages/saloon_3.jpg','src/imgs/saloonImages/saloon_4.jpg','src/imgs/saloonImages/saloon_5.jpg'];
+    const timeless=['src/imgs/timelessImage/timeless.jpg'];
 
 
   init();
@@ -14,12 +22,38 @@
 
   function init(){
       firstDots.forEach(dot => {dot.style.backgroundColor="#e09a2f";})
-      dots.forEach(dot=> dot.addEventListener("click",changeImage));
+        menu.addEventListener("click",showMenu)
+        close.addEventListener("click",showMenu)
+      dots.forEach(dot=> {
+          dot.addEventListener("click",changeImage)
+
+          const key =dot.parentNode.dataset.key
+          if (key == "mp40"){
+              dot.parentNode.style.backgroundImage=`url(${mp40Images[0]})`
+          }
+          if (key == "gameboy"){
+              dot.parentNode.style.backgroundImage=`url(${gameboyImages[0]})`
+          }
+          if (key == "saloon"){
+              dot.parentNode.style.backgroundImage=`url(${saloonImages[0]})`
+          }
+          if (key =="timeless_world"){
+              dot.parentNode.style.backgroundImage=`url(${timeless[0]})`
+
+          }
+      });
+
+
       sectionBtns.forEach(section=> section.addEventListener("click",fitView))
+      hamburgerBtns.forEach(section=> section.addEventListener("click",fitViewBar))
       cvBtn.addEventListener("click", ()=>{
           document.querySelector(".about_sam").scrollIntoView(); })
   }
+    function  showMenu(){
+        console.log(menu_bar)
 
+        menu_bar.classList.toggle("show_menu")
+    }
     function fitView(){
         const target=this.dataset.key;
         console.log(target)
@@ -27,8 +61,13 @@
         document.querySelector(` .category_label[data-key=${target}]`).scrollIntoView();
 
     }
+    function fitViewBar(){
+        const target=this.dataset.key;
+        console.log(target)
+        menu_bar.classList.remove("show_menu")
+        document.querySelector(` .category_label[data-key=${target}]`).scrollIntoView();
 
-  const mp40Images=['src/imgs/mp40Images/mp40_1.jpg','src/imgs/mp40Images/mp40_2.jpg','src/imgs/mp40Images/mp40_3.jpg','src/imgs/mp40Images/mp40_4.jpg']
+    }
 
     function changeImage()
     {
@@ -46,6 +85,16 @@
 
 
             this.parentNode.style.backgroundImage=`url(${mp40Images[dotsArray.indexOf(this)]})`
+        }
+        if (key == "gameboy"){
+
+
+            this.parentNode.style.backgroundImage=`url(${gameboyImages[dotsArray.indexOf(this)]})`
+        }
+        if (key == "saloon"){
+
+
+            this.parentNode.style.backgroundImage=`url(${saloonImages[dotsArray.indexOf(this)]})`
         }
 
     }
